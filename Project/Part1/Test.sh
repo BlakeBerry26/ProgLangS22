304e7284dd83cd650a785ccb4bf6a094820bcf49
Blake Berry <blakeberry.96@gmail.com>
2022-02-28 18:39:06 -0600

    Project Part 1

---
+++ Project/Part1/Test.sh
@@ -0,0 +1,13 @@
+CODE_DIRECTORY=./Code
+TEST_CASE_DIRECTORY=./Test_Cases
+
+clear
+(cd Code && make)
+
+for in_file in $TEST_CASE_DIRECTORY/*.in; do
+    in_file=${in_file::-3}
+    echo $in_file
+    $CODE_DIRECTORY/tips_lex $in_file.in | diff -I '^INFO.*' - $in_file.correct
+done
+
+(cd code && make clean)
